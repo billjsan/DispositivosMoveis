5e9ab59a869626392846c5bad12e603ca4b4df58
Will <wjefferson.santos@gmail.com>
2023-04-26 15:04:36 -0300

    pratica 01 Primeiro app

---
+++ pratica_01/app/src/main/java/pdm/pratica01/MainActivity.java
@@ -0,0 +1,60 @@
+package pdm.pratica01;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MSG = "Pratica01.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MSG, message);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                openSearch();
+                return true;
+            case R.id.action_settings:
+                openSettings();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void openSettings() {
+        Toast.makeText(this, "Não implementada.", Toast.LENGTH_SHORT).show();
+    }
+
+    private void openSearch() {
+        Toast.makeText(this, "Não implementada.", Toast.LENGTH_SHORT).show();
+    }
+}
